69fc28261d9bb21f44c391b94e8853278c9db523
farhandz <ammarjoz09@gmail.com>
2022-03-06 09:44:37 +0700

    first setup

---
+++ lib/contant.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = Color(0xff7B61FF);
+Color priceColor = Color(0xffEB6313);
+Color titleColor = Color(0xff404852);
+Color title2Color = Color(0xff6A452F);
+Color sectionTitle = Color(0x6A452F);
+Color secondaryColor = Color(0xff38ABBE);
+Color promotedColor = Color(0xFFFFFF);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(color: titleColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
